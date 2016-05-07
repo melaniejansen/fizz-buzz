0109fe80ec049b49aeafc4c601d71a02022a3d24
melanie jansen <melanie.weller7@gmail.com>
2016-05-07 00:36:19 -0700

    first commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
 $(document).ready(function() {
+
+	//Click on the submit button to get value
+	$('#submit').on('click', function() {
+		var inputValue = $('#text-input').val();
+
+	//Determine if the number is in range and print value
+		if (inputValue % 1 === 0 && inputValue >= 1 && inputValue <= 100) {
+		fizzbuzz(inputValue);
+		} 
+	});
 	
-		for ( var i = 1; i <= 100; i++ ) {
-		
+});
+
+function fizzbuzz(num) {
+	for ( var i = 1; i <= num; i++ ) {
 		console.log(i);
+
 		if ( i % 15 == 0 ) {
-			$("body").append("fizz buzz");
-			$("body").append("<br/>");
+			console.log("fizz buzz");
+			$(".output").append("fizz buzz" + "<br/>");
 		}
 		else if ( i % 3 == 0 ) {
-			$("body").append("fizz");
-			$("body").append("<br/>");
+			console.log("fizz");
+			$(".output").append("fizz" + "<br/>");
 		}
 		else if ( i % 5 == 0 ) {
-			$("body").append("buzz");
-			$("body").append("<br/>");
+			console.log("buzz");
+			$(".output").append("buzz" + "<br/>");	
 		} 
-
 		else {
 			console.log(i);
-			$("body").append(i);
-			$("body").append("<br/>");
+			$(".output").append(i + "<br/>");
+
 		}
 	}
-});
+};
